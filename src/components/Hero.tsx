0bd326d0424f5151514e3c3c9f3741c7349a5216
Sakkeer A <sakkeer@mongrov.com>
2025-09-13 11:37:01 +0530

    Refactor Hero section styles and layout; add new hero image and responsive design adjustments

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import heroPic from '../assets/images/heroPic.png';
 
 const Hero: React.FC = () => {
   return (
     <section id="home" className="hero">
-      <div className="">
+      <div className="hero-content">
         <div className="hello-badge">
           <span>Hello!</span>
         </div>
         <div className="hero-intro">
           <h1 className="hero-name"><span className="hero-name-highlight">I'm</span> Sakkeer<span className="hero-name-highlight">,</span></h1>
           <h2 className="hero-title">Frontend Developer</h2>
+        </div>
+        <div className="hero-circle">
+          <div className="circle-fill">
+            <img src={heroPic} alt="Sakkeer" className="hero-image" />
+          </div>
         </div>
       </div>
     </section>
